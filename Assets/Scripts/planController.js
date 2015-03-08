285e9af856093b51d5bbfdb91b327beb94e645b5
mingchaoyan <mingchaoyan@gmail.com>
2015-03-08 16:07:44 +0800

    添加控制飞机代码

---
+++ Assets/Scripts/planController.js
@@ -0,0 +1,10 @@
+﻿var speed:int= 2;
+function Update () {
+	if(Input.GetAxis("Horizontal") > 0)
+		transform.Translate(Vector3.right*Time.deltaTime*speed);
+	if(Input.GetAxis("Horizontal") < 0)
+		transform.Translate(2*Vector3.left*Time.deltaTime*speed);
+	
+	transform.Translate(Input.GetAxis("Vertical")*Vector3.up*Time.deltaTime*speed);
+
+}
