63de32242fb9d040f80d85caeb1d24fc012f8ca6
mingchaoyan <mingchaoyan@gmail.com>
2015-03-08 15:56:43 +0800

    螺旋桨动画

---
+++ Assets/Scripts/propellerController.js
@@ -0,0 +1,7 @@
+﻿var index:int = 0;
+var speed:int = 20;
+function Update () {
+	index = Time.time*20%3;
+	renderer.material.mainTextureScale = Vector2(1.0/3.0, 1);
+	renderer.material.mainTextureOffset = Vector2(index*1.0/3.0, 1);
+}
