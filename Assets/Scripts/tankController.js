823716907488ffc4a5ed9871154a9220bd695bbf
mingchaoyan <mingchaoyan@gmail.com>
2015-03-14 18:05:50 +0800

    添加坦克在场景中移动代码

---
+++ Assets/Scripts/tankController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var maxSpeed:float = 3.0;
+var minSpeed:float = 1.0;
+var curSpeed:float =0;
+function Start () {
+	curSpeed = Random.Range(minSpeed, maxSpeed);
+}
+
+function Update () {
+	transform.Translate(Vector3.left * curSpeed*Time.deltaTime);
+	if(transform.localPosition.x < -10){
+		transform.Translate(Vector3(20, 0, 0));
+		Start();
+	}
+
+}
