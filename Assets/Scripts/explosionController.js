678756d12ece58cec9d3715f747395e5622643e8
mingchaoyan <mingchaoyan@gmail.com>
2015-03-15 16:49:03 +0800

    添加爆炸效果

---
+++ Assets/Scripts/explosionController.js
@@ -0,0 +1,14 @@
+﻿private var aniSprite;
+private var isPlaying:boolean = true;
+var explision:AudioClip;
+function Start () {
+	aniSprite = GetComponent("aniSprite");
+	var audioPosition = GameObject.Find("Camera").transform.position;
+	AudioSource.PlayClipAtPoint(explision, audioPosition);
+}
+
+function Update () {
+	if(isPlaying) {
+		isPlaying = aniSprite.aniSprite(4, 6, 0, 0, 10, 10, 2);
+	}
+}
