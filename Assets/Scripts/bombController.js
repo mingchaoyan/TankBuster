e17a0458b73b3e290d887ba5268ccfc0c9b85557
mingchaoyan <mingchaoyan@gmail.com>
2015-03-15 19:08:34 +0800

    炮弹碰撞检测

---
+++ Assets/Scripts/bombController.js
@@ -0,0 +1,22 @@
+﻿var explosion:GameObject;
+var bombSound:AudioClip;
+private var myExplosion;
+
+function Start () {
+	var audioPosition = GameObject.Find("Camera").transform.position;
+	AudioSource.PlayClipAtPoint(bombSound, audioPosition);
+}
+
+function OnCollisionEnter(collision:Collision) {
+	if(collision.gameObject.tag == "tank")
+	{
+		myExplosion = Instantiate(explosion, Vector3(
+			transform.position.x, transform.position.y,
+			-1), Quaternion.identity);
+		Destroy(myExplosion, 0.5);
+		Destroy(gameObject);
+	}
+	if(collision.gameObject.tag == "projectile"){
+		Destroy(collision.gameObject);
+	}
+}
