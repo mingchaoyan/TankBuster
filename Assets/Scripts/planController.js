2bb50ca4c500ea839d9d4f21e962337709892c4b
mingchaoyan <mingchaoyan@gmail.com>
2015-05-31 15:10:48 +0800

    完成飞机炮弹

--- Assets/Scripts/planController.js
+++ Assets/Scripts/planController.js
@@ -1,4 +1,6 @@
 ﻿var speed:int= 2;
+var bomb : Rigidbody;
+var myBomb:Rigidbody;
 function Update () {
 	if(Input.GetAxis("Horizontal") > 0)
 		transform.Translate(Vector3.right*Time.deltaTime*speed);
@@ -6,5 +8,11 @@ function Update () {
 		transform.Translate(2*Vector3.left*Time.deltaTime*speed);
 	
 	transform.Translate(Input.GetAxis("Vertical")*Vector3.up*Time.deltaTime*speed);
+	
+	if(Input.GetKeyDown("space")){
+		print("space down");
+		var fwd = transform.TransformDirection(Vector3.down);
+		myBomb = Instantiate(bomb, transform.position+fwd, transform.rotation);
+	}
 
 }
